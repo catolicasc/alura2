de6d630aed6b1042881c9bd0984e33c36d8a3be6
Luiz Almeida Júnior <allmeidajr@gmail.com>
2017-10-23 01:57:04 -0200

    adicionado erro de http

--- javascript/v3/js/buscar-paciente.js
+++ javascript/v3/js/buscar-paciente.js
@@ -1,23 +1,26 @@
 var botao = document.querySelector('#buscar-paciente')
 // https://api-pacientes.herokuapp.com/pacientes
-botao.addEventListener('click', function(){
-  console.log('https://api-pacientes.herokuapp.com/pacientes');
-  var url =  'https://api-pacientes.herokuapp.com/pacientes'
+botao.addEventListener('click', function() {
+  var url = 'https://api-pacientes.herokuapp.com/pacientes'
   var xhr = new XMLHttpRequest()
-  xhr.open("GET", url)
-  xhr.addEventListener("load", function(){
-    console.log();
-    var resposta = xhr.responseText
-    // resposta = JSON.stringify(reposta)
-    var pacientes = JSON.parse(resposta)
 
-    pacientes.forEach(function(paciente){
-      adicionarPacienteTabela(paciente)
-    })
-    console.log(typeof pacientes);
+  xhr.open("GET", url)
+  xhr.addEventListener("load", function() {
+    if (xhr.status == 200) {
+      console.log();
+      var resposta = xhr.responseText
+      var pacientes = JSON.parse(resposta)
+      pacientes.forEach(function(paciente) {
+        adicionarPacienteTabela(paciente)
+      })
+    } else {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+      var erro = document.querySelector('#erro-ajax')
+      erro.classList.remove('invisivel')
+    }
 
   })
-
   xhr.send()
   // enviado a requisão acima, agora deve ser usada a resposta da requisao
 })
